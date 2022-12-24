db7dd14222cb09ca1df117d9b1954eea9b42f604
macc80 <96655607+macc80@users.noreply.github.com>
2022-12-25 00:01:32 +0200

    added search engine

--- src/index.js
+++ src/index.js
@@ -49,7 +49,20 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "836f4ef75ccee844372d8db2b2350564";
-let city = "Bulawayo";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "836f4ef75ccee844372d8db2b2350564";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#form-search");
+form.addEventListener("submit", handleSubmit);
+
+search("Bulawayo");
